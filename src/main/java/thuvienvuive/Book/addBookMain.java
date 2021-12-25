65432c9f7794e805dda7d4ec511a5ee9dc4434c6
Lý Thế Minh <lytheminh3q@gmail.com>
2021-12-25 20:35:37 +0700

    AddBook AddMember SelectAuthor

--- src/main/java/thuvienvuive/Book/addBookMain.java
+++ src/main/java/thuvienvuive/Book/addBookMain.java
@@ -6,7 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import thuvienvuive.Book.BookListController;
-
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class addBookMain extends Application {
@@ -14,8 +15,10 @@ public class addBookMain extends Application {
     public void start(Stage stage) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(AddBookController.class.getResource("AddNewBook.fxml"));
+            
             Scene scene = new Scene(fxmlLoader.load());
             stage.setScene(scene);
+            stage.initStyle(StageStyle.UNDECORATED);
             stage.show();
         } catch(Exception e) {
             e.printStackTrace();
--- src/main/java/thuvienvuive/Member/addMemberMain.java
+++ src/main/java/thuvienvuive/Member/addMemberMain.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,6 +15,7 @@ public class addMemberMain extends Application{
             Scene scene = new Scene(fxmlLoader.load());
             scene.getStylesheets().add(getClass().getResource("deleteMember.css").toExternalForm());
             stage.setScene(scene);
+            stage.initStyle(StageStyle.UNDECORATED);
             stage.show();
         } catch(Exception e) {
             e.printStackTrace();
